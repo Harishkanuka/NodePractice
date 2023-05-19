4e063325e20f63234c38841500902bea40e741e6
Your Name <kanukaharish@gmail.com>
2023-05-19 07:49:04 +0530

    Created Own Middleware

--- 02.Contact_list/index.js
+++ 02.Contact_list/index.js
@@ -6,6 +6,21 @@ const app = express();
 // Set up the EJS for views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+//Creating a middleware to handle encoding for that we are using parser which is express inbuit funtion
+app.use(express.urlencoded()); //This will encode data in object format
+
+//Setting up our own middlewares
+//MiddleWARE 01
+app.use(function (req, res, next) {
+	console.log('Middleware1 is called');
+	next();
+});
+
+//Middleware 02
+app.use(function (req, res, next) {
+	console.log('Middleware2 is called');
+	next();
+});
 
 app.get('/', function (req, res) {
 	// console.log(__dirname);
@@ -22,7 +37,16 @@ app.get('/practice', function (req, res) {
 });
 
 app.post('/create-contact', function (req, res) {
-	return res.redirect('/practice');
+	//append data into array
+	// contactList.push({
+	// 	name: req.body.name,
+	// 	phone: req.body.phone,
+	// });
+	// same as above code
+	contactList.push(req.body);
+	// return res.redirect('/');
+	//can also use back instead of url
+	return res.redirect('back');
 });
 
 var contactList = [
